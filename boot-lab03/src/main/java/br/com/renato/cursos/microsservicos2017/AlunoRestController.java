3ee12fc212c215354deb56eebb31d10fca0e7314
Renato Passos Santos <renato.passos@tse.jus.br>
2017-09-11 15:45:10 -0300

    Removendo import desnecessário.

--- boot-lab03/src/main/java/br/com/renato/cursos/microsservicos2017/AlunoRestController.java
+++ boot-lab03/src/main/java/br/com/renato/cursos/microsservicos2017/AlunoRestController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.val;
-
 @RestController
 @RequestMapping("/alunos")
 public class AlunoRestController {
